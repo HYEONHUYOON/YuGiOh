0c7187ad280b5f5573d2a4f142bb3d12873db1ca
HYEONHU YOON <hhmkyh234@gmail.com>
2024-08-21 23:31:56 +0900

    [Add] middleware

---
+++ cards/src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+ 
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+
+  if(path === '/'){
+    return NextResponse.redirect(new URL('/home', request.url));
+  }
+}
+ 
+// Matcher Config
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|.+?/hook-examples|.+?/menu-examples|images|next.svg|vercel.svg).*)'
+  ]
+}
