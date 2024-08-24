97108d5cb3544186841812e8265ed63b3a8b8548
HYEONHU YOON <hhmkyh234@gmail.com>
2024-08-24 14:25:00 +0900

    [Add] font config

--- cards/src/styles/theme.tsx
+++ cards/src/styles/theme.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import { createTheme } from '@mui/material/styles';
+import '../assets/fonts/font.css'
 
 export const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#ff8e88',
+    typography:{
+        fontFamily: 'Small_Caps_1'
     },
-  },
+    palette: {
+        primary: {
+        main: '#ff8e88',
+        },
+    },
 });
