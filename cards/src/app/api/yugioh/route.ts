558c094e8e4cbe57a11a8c6b81c79213ef97883f
HYEONHU YOON <hhmkyh234@gmail.com>
2024-08-24 14:25:20 +0900

    [Del] console log

--- cards/src/app/api/yugioh/route.ts
+++ cards/src/app/api/yugioh/route.ts
@@ -5,8 +5,6 @@ export async function GET(request: Request) {
     const response = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark Magician');
     const result = await response.json();
 
-    console.log(result.data);
-
     // 여기에 NextResponse를 반환합니다.
     return NextResponse.json({ msg: 'Hello from server', data: result.data });
   } catch (error : any) {
