7139d585531f349c3f85cb309dd475ba4382a945
HYEONHU YOON <hhmkyh234@gmail.com>
2024-08-25 15:51:48 +0900

    [add] file loader next config

--- cards/next.config.js
+++ cards/next.config.js
@@ -3,8 +3,23 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-    images: {
-      domains: ['images.ygoprodeck.com'],
-    },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp3|wav|ogg)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[hash].[ext]',
+          outputPath: 'static/media/', // 파일이 저장될 경로
+          publicPath: '/_next/static/media/', // Next.js가 접근할 수 있는 경로
+        },
+      },
+    });
+
+    return config;
+  },
+  images: {
+    domains: ['images.ygoprodeck.com'],
+  },
 };
   
